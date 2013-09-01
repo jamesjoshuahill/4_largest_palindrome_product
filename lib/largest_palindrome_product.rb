3c89cc4b22a4310ef8c8c42466f0965b0331ab76
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-01 15:30:20 +0100

    Made alternative implementation more efficient

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -6,39 +6,23 @@ class LargestPalindromeProduct
 
   def largest_palindrome(digits)
     largest_number = ("9" * digits).to_i
-    #iterate through products down from largest_number
-    #squared until a palindrome is found
-    # products = []
-    # largest_number.downto(largest_number - 10) do |num|
-    #   break if num == 0
-    #   0.upto(100) do |interval|
-    #     break if largest_number - interval == 0
-    #     # puts "#{num} * #{largest_number - interval} = #{num * (largest_number - interval)}"
-    #     products.push(num * (largest_number - interval))
-    #   end
-    # end
+    largest_palindrome = 0
 
-    # products.select { |num| palindrome?(num) }.max
-
-    #Alternative implementation
-    palindromes = [0]
     largest_number.downto(1) do |a|
       largest_number.downto(a) do |b|
         product = a * b
-        # puts "#{a} * #{b} = #{product}"
+        break if product < largest_palindrome
         if palindrome?(product)
-          palindromes.push(product)
+          largest_palindrome = product if product > largest_palindrome
           break
         end
       end
-      # puts "--------"
-      break if ((a * largest_number) < palindromes.max)
+      break if ((a * largest_number) < largest_palindrome)
     end
-    palindromes.max
-
+    largest_palindrome
   end
 
 end
 
 #simple execution test
-#puts LargestPalindromeProduct.new.largest_palindrome(3)
+puts LargestPalindromeProduct.new.largest_palindrome(3)
